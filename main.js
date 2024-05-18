d53250603f7047779350e8fdadd05f1af447b63f
saran-narayana <nsaran030201@gmail.com>
2024-05-18 20:25:22 +0530

    Javascript code

--- main.js
+++ main.js
@@ -55,6 +55,7 @@
 
 
 //               4 .Arrays
+/*
  const cars = new Array("Saab", "Volvo", "BMW");
  console.log(cars);
 
@@ -75,12 +76,13 @@ let position = fruits.indexOf("Apple") + 1;
 console.log("Apple is found in position  "+position);
 fruits.includes("Mango");
 console.log(fruits);
+*/
 
 
 
 // // 5.conditional statement
 //        // if and else   //
-var time = 24;
+/*var time = 24;
 
    if(time < 30)
     {
@@ -127,13 +129,13 @@ else if(day == 6){
 else{
     console.log("it is sunday");
 }
-
+*/
 
 //6.looping statement
 
-//*for loop
+//for loop
 // let i;
-// for (let i = 0; i < 5; i++) {
+// for (let i = -50; i < 0; i++) {
 //     console.log(i);  
 // }
 
@@ -141,7 +143,7 @@ else{
 // const person = {fname:"John", lname:"Doe", age:25}; 
 // for(let x in person){
 //     console.log(person [x] + " " );
-// }
+// 
 
 //for of loop
 
@@ -157,7 +159,171 @@ else{
 //     i=i+2;
 // }
 
+              //  7. break and continue statement  //
+ 
+        //  for (i=0;i<10;i++)
+        //     {
+        //         console.log(i);
+        //         if(i == 5){
+        //             //break;
+        //             continue;
+        //         }
+        //     }    
+            
+            // 8. set method()
+              //add()method
+//  const letter = new Set();
+//  letter.add("a");
+//  letter.add("b");
+//  letter.add("c");
+
+//  console.log("the set has "+ letter.size +" values.");
+
+
+//  const letters = new Set(["a","b","c","d"]);
+//  let text ="";
+//  for(const x of letters){
+//     console.log(text += x );
+//  }
+  
+//*has()method//
+// const letters = new Set(["a","b","c"]);
+// answer = letters.has("d");
+// console.log("The answer is " + answer);
+
+// *forEach() method//
+// const letter = new Set(["a","b","c"]);
+// letters.forEach (function(value) {
+//  console.log(value);
+//   })
+
+  //values() method//
+
+  // const letter = new Set(["a","b","c"]);
+  // const myIterator = letter.values();
+  // for(const entry of myIterator){
+  //   console.log(entry);
+  // }
+
+
+  // 9.map 
+  //*new map()  passing tht array to new map()
+//   const fruits = new Map([
+//     ["apples", 500],
+//     ["bananas", 300],
+//     ["oranges", 200]
+//   ]);
+//   let numb = fruits.get("apples");
+//   console.log("There are "+numb+" apples");
+
+// //*set() and get() method
+// const fruit = new Map();
+// fruit.set("apples", 500);
+// fruit.set("bananas", 300);
+// fruit.set("oranges", 200);
+// let num = fruit.get("oranges");
+// console.log("There are "+num+ " oranges");
+
+//map method
+
+// const fruits = new Map([
+//     ["apples", 500],
+//     ["bananas", 300],
+//     ["oranges", 200]
+//   ]);
+  // * map.size() //
+//fruits.size; 
+
+//  *map.delete() //
+//fruits.delete("apples");
+//console.log(fruits);
+
+// *map.entries() //
+// for(const x of fruits.entries()){
+//  console.log(x);
+// }
+  
+
+
+// 10. type conversion //
+ 
+//Implicit conversion - automatic
+//Explicit conversion - manual
+
+// let result;
+// //Implicit conversion
+// result = "3" + 2; 
+// console.log(result, "-", typeof(result));
+
+// result = "3" + true; 
+// console.log(result, "-", typeof(result));
+
+// result = "3" + null; 
+// console.log(result, "-", typeof(result));
+
+
+// // explicit conversion
+
+// let x = "k";
+// let y = "pizza";
+// let z = "0";
+
+
+// x = Number(x);
+// y = String(y);
+// z = Boolean(z);
+
+// console.log(x, typeof x);
+
+// console.log(y, typeof y);
+
+// console.log(z, typeof z);
 
 
 
+// 11. errors
+// const numerator= 100, denominator = a;
 
+// try {  //
+//      console.log(numerator/denominator);
+    
+//     console.log(a);
+// }
+// catch(error) { //
+//     console.log('An error caught'); 
+//     console.log('Error message: ' + error);  
+// }
+
+// finally{  //
+//     console.log('Finally will execute every time');
+// }
+
+//throw
+// const number = 40;
+// try {
+//     if(number > 50) {
+//         console.log('Success');
+//     }
+//     else {
+//         throw new Error('The number is low'); //
+//     }
+//     console.log('hello');
+// }
+// catch(error) {
+//     console.log('An error caught'); 
+//     console.log('Error message: ' + error);  
+// }
+
+//  12. Arrow function
+
+// const addNumbers = (a, b) => a + b;
+// const result = addNumbers(5, 3);
+// console.log(result);
+
+// //arrow function with no argument
+// const sayHello = () => "Hello, World!";
+// console.log(sayHello());  
+
+// //arrow function with one argument
+// const square = x => x * x;
+// console.log(square(5));
