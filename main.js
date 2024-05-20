9cc4dd179b5c26576aa62fa430397c5b24a1cf23
saran-narayana <nsaran030201@gmail.com>
2024-05-20 19:54:52 +0530

    Javascript code

--- main.js
+++ main.js
@@ -557,7 +557,21 @@ else{
 // console.log(student.firstName);
 
 
+// 19.Recursion
 
+function counter(count) {
+
+  console.log(count);
+
+  if(count > 1) {
+
+      count = count - 1;
+      counter(count);
+  } else {
+      return;
+  };
+};
+counter(5);
 
 
 
